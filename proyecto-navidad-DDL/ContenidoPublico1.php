11a14b9e0d6f76eec12d88911d0ee807b22823c6
dav1dortiz <72920908+dav1dortiz@users.noreply.github.com>
2021-01-04 13:22:11 +0100

    Add files via upload

--- proyecto-navidad-DDL/ContenidoPublico1.php
+++ proyecto-navidad-DDL/ContenidoPublico1.php
@@ -1,2 +1,46 @@
+//David
+
 <?php
-//listado de vuelos 1
+require_once "-com/Varios.php";
+require_once "-com/Dao.php";
+
+$vuelos=DAO::vueloObtenerTodas();
+?>
+
+<html>
+<head> <meta charset="UTF-8"> </head>
+<body>
+<h1>Listado de Vuelos</h1>
+
+<table border='1'>
+    <tr>
+        <th>Vuelo</th>
+        <th>Origen</th>
+        <th>Destino</th>
+        <th>Fecha ida</th>
+        <th>Fecha vuelta</th>
+        <th>Reservar</th>
+
+    </tr>
+    <tr>
+        <?php
+        foreach ($vuelos as $vuelo) { ?>
+    <tr>
+        <td> <?=$vuelos-> getVuelo()?>    </td>
+        <td> <?=$vuelos-> getOrigen()?>   </td>
+        <td> <?=$vuelos-> getDestino()?> </td>
+        <td> <?=$vuelos-> getFecha()?> </td>
+        <td><a href="SesionInicioMostrarFormulario.php"> (X) </a> </td>
+
+
+    </tr>
+    <?php } ?>
+    </tr>
+</table>
+
+<br><br>
+
+
+</body>
+</html>
+
