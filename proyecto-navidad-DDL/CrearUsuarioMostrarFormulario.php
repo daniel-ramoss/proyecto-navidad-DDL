15628414cfc3455e3fc1c42926669f692005808c
dav1dortiz <72920908+dav1dortiz@users.noreply.github.com>
2021-01-05 12:41:52 +0100

    Crear usuario 05/1/21

--- proyecto-navidad-DDL/CrearUsuarioMostrarFormulario.php
+++ proyecto-navidad-DDL/CrearUsuarioMostrarFormulario.php
@@ -1,2 +1,29 @@
 <?php
-//formulario para la creación del usuario en caso de que no exista
+require_once "-com/Varios.php";
+require_once "-com/Dao.php";
+?>
+
+
+
+<html>
+
+<head>
+    <meta charset='UTF-8'>
+</head>
+
+
+
+<body>
+
+<h1>Crear Usuario</h1>
+
+<form action='SesionInicioComprobar.php' method='get'>
+    <p>Usuario: <input type='text' name='identificador' /></p>
+    <p>Contraseña: <input type='password' name='contrasenna' /></p>
+    <input type='submit' name='boton' value="Enviar" />
+</form>
+
+
+</body>
+
+</html>
