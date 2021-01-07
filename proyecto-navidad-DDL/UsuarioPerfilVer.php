60d9ba8f01afd603b0e1c3bdc1ff00c8ad384046
dav1dortiz <72920908+dav1dortiz@users.noreply.github.com>
2021-01-07 12:43:45 +0100

    Usuario Perfil(David 07/01/21)

--- proyecto-navidad-DDL/UsuarioPerfilVer.php
+++ proyecto-navidad-DDL/UsuarioPerfilVer.php
@@ -1,2 +1,30 @@
+//David
 <?php
-//se muestran las caracteristicas del usuario que ha iniciado la session
+require_once "-com/Dao.php";
+require_once "-com/Clases.php";
+
+if (DAO::haySesionIniciada()){
+    $_nombre=$_SESSION['nombre'];
+    $_apellidos=$_SESSION['apellidos'];
+    $_identificador=$_SESSION['identificador'];
+}else
+    redireccionar("CrearUsuarioMostrarFormulario.php");
+?>
+
+<html>
+<head>Perfil Usuario</head>
+
+<body>
+<h3>Nombre: <?=$_nombre?> </h3>
+<h3>Apellidos: <?=$_apellidos?> </h3>
+<h3>Usuario: <?=$_identificador?> </h3>
+<br>
+<br>
+<a href="SesionCerrar.php">Cerrar sesión.</a><br><br>
+<a href="FormularioReservaVuelos.php">Página principal.</a><br><br>
+<a href="ListadoVuelosCompleto.php">Ver Vuelos.</a><br><br>
+
+
+
+</body>
+</html>
